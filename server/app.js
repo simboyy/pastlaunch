8dd51ea18ec03ed03a6fe8a30b3cbabf4f8e2747
simboyy <smkorera@gmail.com>
2017-05-22 00:52:48 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ _mongoose2.default.Promise = require('bluebird');
 
 
 // Connect to MongoDB
-_mongoose2.default.connect('mongodb://127.0.0.1:27017/mediaboxzim');
+_mongoose2.default.connect('mongodb://simba:1994kingsss@ds135797.mlab.com:35797/mediaboxzim');
 _mongoose2.default.connection.on('error', function (err) {
   console.error('MongoDB connection error: ' + err);
   process.exit(-1);
@@ -41,6 +41,7 @@ if (_environment2.default.seedDB) {
   require('./config/seed');
 }
 
+
 // Setup server
 var app = (0, _express2.default)();
 var server = _http2.default.createServer(app);
@@ -48,13 +49,11 @@ var socketio = require('socket.io')(server, {
   serveClient: _environment2.default.env !== 'production',
   path: '/socket.io-client'
 });
-require('./config/socketio').default(socketio);
-require('./config/express').default(app);
-require('./routes').default(app);
+
 
 // Start server
 function startServer() {
-  app.angularFullstack = server.listen(_environment2.default.port, _environment2.default.ip, function () {
+  app.angularFullstack = server.listen(process.env.PORT, function () {
     console.log('Express server listening on %d, in %s mode', _environment2.default.port, app.get('env'));
   });
 }
